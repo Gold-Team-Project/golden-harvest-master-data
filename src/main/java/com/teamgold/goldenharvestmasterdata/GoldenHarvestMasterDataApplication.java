d5c3c1e6078edd73f20b17252bdb8bca5487418e
KyujinPark01 <rbwls1100@naver.com>
2026-01-30 14:38:56 +0900

    Initial commit

---
+++ src/main/java/com/teamgold/goldenharvestmasterdata/GoldenHarvestMasterDataApplication.java
@@ -0,0 +1,13 @@
+package com.teamgold.goldenharvestmasterdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldenHarvestMasterDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoldenHarvestMasterDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamgold/goldenharvestmasterdata/GoldenHarvestMasterDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamgold.goldenharvestmasterdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenHarvestMasterDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
